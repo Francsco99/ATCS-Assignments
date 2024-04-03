2661878ac4cbfc428d4079aeca552c219c26c726
fspezzano <fra.spezzano@stud.uniroma3.it>
2024-04-03 18:47:51 +0200

    moved files

--- sequential-recommendations/test.py
+++
@@ -1,9 +0,0 @@
-import recommender as rc
-import sequential as seq
-import plots as pt
-df = rc.open_file('data/ml-latest-small/ratings.csv')
-group=[493, 26, 57]
-group_sat=seq.sequential_recommender(group,4,df,10)
-seq.save_to_csv(group,group_sat,"data/csv/homo.csv")
-pt.plot_subgraphs('data/csv/homo.csv','data/plots/homo-sat.png')
-pt.plot_group_scores('data/csv/homo.csv','data/plots/homo-dis-sat.png')
